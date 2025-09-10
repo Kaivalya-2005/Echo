abc89d25fadd263d43ef237c1f7295fc43d29f85
Kaivalya-2005 <kaivalya.deshpande2005@gmail.com>
2025-09-10 10:27:46 +0530

    start sound added

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import os
 import eel
 
-eel.init('www')
+from engine.features import *
 
+eel.init('www')
 os.system('start msedge.exe --app="http://127.0.0.1:5500/www/index.html"')
+playAssistantSound()
 
 eel.start('index.html', mode=None, host='localhost',block=True)
